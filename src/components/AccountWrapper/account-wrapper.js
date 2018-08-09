608902f8d5937c797d0cf5b87f0df259cbb86013
Dyachenko R <antinaar@gmail.com>
2018-08-10 00:22:07 +0300

    add AccountWrapper

---
+++ src/components/AccountWrapper/account-wrapper.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const AccountWrapper = styled.div`
+  width: 400px;
+  margin: 100px auto;
+  background: #FCEC52;
+  padding: 60px 60px 30px;
+  position: relative;
+  box-shadow: 0 22px 50px rgba(14,21,47, .2), 0 8px 20px rgba(14,21,47, .2);
+`;
+
+export default AccountWrapper;
