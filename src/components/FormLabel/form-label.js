9087ba11750fdbc49e5a8d078b48982f16ea149d
Dyachenko R <antinaar@gmail.com>
2018-08-10 20:33:18 +0300

    add FormLabel

---
+++ src/components/FormLabel/form-label.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export default styled.label`
+  color: #aaa;
+  text-transform: uppercase;
+  margin-bottom: 0;
+  font-size: 12px;
+  font-weight: 500;
+`;
