a7ee687a20bfa851f3f63a633cda41a8f5e6dce9
Dyachenko R <antinaar@gmail.com>
2018-08-11 13:05:19 +0300

    add AccountForm to App

--- src/components/App/app.js
+++ src/components/App/app.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import AccountWrapper from '../AccountWrapper';
 import AccountCardTitle from '../AccountCardTitle'
+import AccountCardForm from '../AccountCardForm';
 
 export default () => (
   <AccountWrapper>
     <AccountCardTitle/>
+    <AccountCardForm/>
   </AccountWrapper>
 )
